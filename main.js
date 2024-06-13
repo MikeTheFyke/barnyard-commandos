128a23d4b084f53b87aadfb9de2aaba8042ed69a
MikeTheFyke <mikefyke@hotmail.com>
2024-06-13 13:52:59 -0400

    Building main template'

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ kaboom({
 	height: 720,
 	letterbox: true,
 });
+
+const scenes = {
+	menu: () => {
+		add([text("Test"), pos(640, 360)]);
+	},
+	controls: () => {},
+	// Levels 1, 2 & 3
+	1: () => {},
+	2: () => {},
+	3: () => {},
+	gameover: () => {},
+	end: () => {},
+};
+
+// Instead of manually calling each scene object we will map though to call all of them in one loop
+for (const key in scenes) {
+	scene(key, scenes[key]);
+}
+
+// Kaboom requires a default screen to run, here i will use menu
+go("menu");
